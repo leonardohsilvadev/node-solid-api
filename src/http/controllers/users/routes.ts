8fbbe19e6f800fe86c5c2ff225a0d104cf38f89e
Leonardo Silva <leonardohsilva.dev@gmail.com>
2024-06-10 20:37:47 -0300

    feat: add refresh token controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   /* Unauthenticated Routes */
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /* Authenticated Routes */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
